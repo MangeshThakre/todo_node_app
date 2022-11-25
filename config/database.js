b89eaa04f126ed90e7a0880f669759c04ddf4857
Mangesh Thakre <mangeshthakre933@gmail.com>
2022-11-25 18:30:35 +0530

    added // unhandled promiss rejection

--- config/database.js
+++ config/database.js
@@ -3,16 +3,18 @@ const MONGODB_URL = process.env.MONGODB_URL;
 
 const mongoDbconnection = () => {
   mongoose
-    .connect(MONGODB_URL, {
-      //must add in order to not get any error masseges:
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      useCreateIndex: true,
-    })
+    .connect(
+      MONGODB_URL
+      //    {
+      //   //must add in order to not get any error masseges:
+      //   useUnifiedTopology: true,
+      //   useNewUrlParser: true,
+      //   useCreateIndex: true,
+      // }
+    )
     .then((data) =>
       console.log(`successfuly connected to DB ${data.connection.host}`)
-    )
-    .catch((error) => console.log(error));
+    );
 };
 
 module.exports = mongoDbconnection;
